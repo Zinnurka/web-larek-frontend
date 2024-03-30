cfe0cc08bb0632cbb934df3bc8ce195aeab2f86c
Zinnurov Ramil <zinnurov.ramil@infotecs.ru>
2024-03-30 09:56:23 +0500

    Пробный запрос по API

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 import './scss/styles.scss';
 
+import {Api} from "./components/base/api"
+import {API_URL} from './utils/constants';
+
+
+const api = new Api(API_URL)
+const product = '/product'
+
+const productCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog')
+
+
+// Получаем список товаров с сервера
+const products = api.get(product)
+console.log(products)
+
